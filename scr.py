66175790948220f5c92b71467556d1f49cb062d1
mattura <user@server.fake.n00b>
2013-04-22 14:44:03 +0100

    scr.py update scrolling fast

--- scr.py
+++ scr.py
@@ -15,10 +15,6 @@ def show_scr(scrollinglist,index):
    scrollinglist.ssd1306.display_block(scrollinglist.bitmaps[index],0,0,scrollinglist.cols)
    scrollinglist.position=index*32
 
-def draw_p(scr,x,a):
-  scr.bitmaps[0].data[(x+a)*4-1]=0x00
-  scr.bitmaps[0].data[x*4-1]=0xFF
-
 def draw_text(bmp, x, y, string):
   font=font5x8.Font5x8
   font_bytes = font.bytes
@@ -48,37 +44,24 @@ def draw_pixel(bmp, x, y, on=True):
 
 led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
 led.begin()
-led.clear_display()
+scr = gaugette.ssd1306.SSD1306.ScrollingList(led,[""],verdana_15)
+bmp=gaugette.ssd1306.SSD1306.Bitmap(1200,32) #new bmp of fixed width
+draw_text(bmp,0,0,"TITLE")
+draw_text(bmp,0,16,"Scroller /\ (ascii set)")
+draw_text(bmp,0,24,"Stationary text")
+scr.bitmaps=[bmp]
 
-sfont=verdana_15
+y=1		#column for scroller
+char_sep=15	#separation between characters
+alph=gaugette.ssd1306.SSD1306.Bitmap(95*char_sep+128,8) #buffer holds alpha chars
+for i in range(0,95):  #draw alpha chars in buffer
+ draw_text(alph,64+i*char_sep,0,chr(i+32))
 
-led.draw_text3(0,0,"Loading...",sfont)
-led.display()
-
-scr = gaugette.ssd1306.SSD1306.ScrollingList(led,["All your base are belong to us!"],sfont)
-#led.invert_display()
-
-draw_text(scr.bitmaps[0],0,0,"Hello test, we are off to see the wizard")
-
-wid=scr.bitmaps[0].cols-128
-str=0
-scr.ssd1306.display_block(scr.bitmaps[0],0,0,128,str) #side scroll
-
-time.sleep(1)
+wid=alph.cols-128
+scr.ssd1306.display_block(scr.bitmaps[0],0,0,128,0) #show screen
 
 while True:
- for i in range(str,wid):  #row,col,count(width),offset
-#  scr.ssd1306.display_block(scr.bitmaps[0],0,0,i,0) #wipe l-r
-#  scr.ssd1306.display_block(scr.bitmaps[0],0,-i,i,0) #roll?
-#  scr.ssd1306.display_block(scr.bitmaps[0],0,0,128,0) #standard
-#  scr.ssd1306.display_block(scr.bitmaps[0],0,0,128,i) #side scroll
-#  scr.ssd1306.display_block(scr.bitmaps[0],0,j,i,0) 
-
-  draw_p(scr,63+i,-1)
-  scr.ssd1306.display_block(scr.bitmaps[0],0,0,128,i) #side scroll
- time.sleep(0.1)
- for i in range(wid,str,-1):
-  draw_p(scr,63+i,1)
-  scr.ssd1306.display_block(scr.bitmaps[0],0,0,128,i) #side scroll
- time.sleep(0.1)
-
+ for i in range(0,wid,1):
+  scr.ssd1306.display_block(alph,y*8,0,128,i)
+ for i in range(wid,0,-1):
+  scr.ssd1306.display_block(alph,y*8,0,128,i)
